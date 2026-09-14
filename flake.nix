ba3ae6b62f65064421e67bd2cacd28fd4b523a99
Aidan Chelig <aidan.chelig@gmail.com>
2026-09-13 19:52:39 -0500

    fix: update edbmat for bevy 0.19 APIs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,20 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixgl.url = "github:nix-community/nixGL";
+
+    arbora = {
+      url = "github:Aidan-Chelig/arbora";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+
+    trenchbroom-chaps = {
+      url = "git+ssh://git@github.com/Aidan-Chelig/TrenchBroom-Chaps.git?ref=master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -11,6 +25,9 @@
     {
       self,
       nixpkgs,
+      nixgl,
+      arbora,
+      trenchbroom-chaps,
       flake-utils,
       rust-overlay,
     }:
@@ -20,37 +37,62 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
+          config.allowUnfree = true;
         };
+        trenchbroomPackage = trenchbroom-chaps.packages.${system}.default;
+        trenchbroom = pkgs.writeShellScriptBin "trenchbroom" ''
+          unset LD_LIBRARY_PATH
+          exec ${nixgl.packages.${system}.nixGLIntel}/bin/nixGLIntel ${trenchbroomPackage}/bin/trenchbroom "$@"
+        '';
+        ericwTools = pkgs.stdenv.mkDerivation {
+          pname = "ericw-tools-bin";
+          version = "2.0.0-alpha10";
+
+          src = pkgs.fetchzip {
+            url = "https://github.com/ericwa/ericw-tools/releases/download/2.0.0-alpha10/ericw-tools-2.0.0-alpha10-Linux.zip";
+            hash = "sha256-tDrH11P9DdKx9iUataCEircqWk0DAFZGKmwUBz1k87s=";
+            stripRoot = false;
+          };
+
+          nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+          buildInputs = [
+            pkgs.stdenv.cc.cc.lib
+            pkgs.zlib
+            pkgs.embree
+          ];
+
+          dontConfigure = true;
+          dontBuild = true;
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin $out/lib
+            cp qbsp vis light bsputil bspinfo $out/bin/
+            cp *.so* $out/lib/
+
+            runHook postInstall
+          '';
+        };
+        # 👇 new! note that it refers to the path ./rust-toolchain.toml
         rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustToolchain;
-          rustc = rustToolchain;
-        };
-        cargoManifest = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        arboraPackage = arbora.packages.${system}.default;
       in
       with pkgs;
       {
-        formatter = nixfmt-tree;
-
-        packages.default = rustPlatform.buildRustPackage {
-          pname = "bmat";
-          version = cargoManifest.package.version;
-          src = self;
-
-          cargoLock.lockFile = ./Cargo.lock;
-          nativeBuildInputs = [ makeWrapper ];
-          nativeCheckInputs = [ git ];
-
-          postInstall = ''
-            wrapProgram "$out/bin/ora_to_bmat" \
-              --prefix PATH : "${lib.makeBinPath [ git ]}"
-          '';
-        };
-
         devShells.default = mkShell {
           shellHook = ''
             export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
               pkgs.lib.makeLibraryPath [
+                pkgs.alsa-lib
+                pkgs.udev
+                pkgs.vulkan-loader
+                pkgs.libxkbcommon
+                pkgs.wayland
+                pkgs.libX11
+                pkgs.libXcursor
+                pkgs.libXrandr
+                pkgs.libXi
                 pkgs.libclang.lib
                 pkgs.stdenv.cc.cc.lib
               ]
@@ -58,16 +100,43 @@
             export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
           '';
 
-          packages = [
+          # 👇 we can just use `rustToolchain` here:
+          buildInputs = [
+            cmake
+            arboraPackage
             rustToolchain
             rust-analyzer
             rustfmt
             cargo-edit
             cargo-watch
-            git
+            tracy
+            ericwTools
+            trenchbroom
             pkg-config
+            alsa-lib
+            jack2
+
+            lld
+            clang
+            libclang.lib
+
+            udev
+            # lutris
+            wayland
+            wayland-protocols
+            libxkbcommon
+            libX11
+            libXcursor
+            libXrandr
+            libXi
+            vulkan-tools
+            vulkan-headers
+            vulkan-loader
+            vulkan-validation-layers
+            libjack2
             just
             bacon
+
           ];
         };
       }
--- src/bin/edbmat.rs
+++ src/bin/edbmat.rs
@@ -33,12 +33,12 @@ fn main() {
 fn setup(mut commands: Commands, assets: Res<AssetServer>, state: Res<EditorState>) {
     commands.spawn((
         PreviewCube,
-        Mesh3d(assets.add(Cuboid::from_length(2.0))),
+        Mesh3d(assets.add(Cuboid::from_length(2.0).into())),
         GenericMaterial3d(assets.load(state.path.to_string_lossy().to_string())),
         Transform::from_xyz(0.0, 0.0, 0.0),
     ));
     commands.spawn((Camera3d::default(), Transform::from_xyz(3.5, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y)));
-    commands.spawn((PointLight { intensity: 1800.0, shadows_enabled: true, ..default() }, Transform::from_xyz(3.0, 4.0, 4.0)));
+    commands.spawn((PointLight { intensity: 1800.0, shadow_maps_enabled: true, ..default() }, Transform::from_xyz(3.0, 4.0, 4.0)));
 }
 
 fn rotate_preview(time: Res<Time>, mut cube: Query<&mut Transform, With<PreviewCube>>) {
@@ -51,6 +51,6 @@ fn inspector_ui(mut commands: Commands, state: Res<EditorState>, mut done: Local
     let title = format!("edbmat — {}", state.path.display());
     let details = state.inspection.as_ref().map(|i| format!("BMAT v{}\n\nTextures:\n{}\n\nAlpha: {:?}", i.manifest.version, i.entries.iter().filter(|e| e.ends_with(".ktx2")).map(|e| format!("  {e}")).collect::<Vec<_>>().join("\n"), i.manifest.alpha_mode)).unwrap_or_else(|| "New BMAT project\n\nSave will create this bundle directly.".to_owned());
     commands.spawn(Node { position_type: PositionType::Absolute, right: Val::Px(0.0), top: Val::Px(0.0), width: Val::Px(300.0), height: Val::Percent(100.0), padding: UiRect::all(Val::Px(18.0)), ..default() }).with_children(|parent| {
-        parent.spawn((Text::new(format!("{title}\n\n{details}")), TextFont { font_size: 16.0, ..default() }, TextColor(Color::WHITE)));
+        parent.spawn((Text::new(format!("{title}\n\n{details}")), TextFont { font_size: FontSize::Px(16.0), ..default() }, TextColor(Color::WHITE)));
     });
 }
